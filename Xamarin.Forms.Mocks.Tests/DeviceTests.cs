c90f084e5d677dcfcd752a2ab9018c0b07a96e8c
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-12-27 21:30:36 -0600

    Test for Device.BeginInvokeOnMainThread

---
+++ Xamarin.Forms.Mocks.Tests/DeviceTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Xamarin.Forms.Mocks.Tests
+{
+    [TestFixture]
+    public class DeviceTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            MockForms.Init();
+        }
+
+        /// <summary>
+        /// NOTE: not currently sure if this is the best idea yet or not
+        /// </summary>
+        [Test]
+        public void BeginInvokeOnMainThreadIsSynchronous()
+        {
+            bool success = false;
+            Device.BeginInvokeOnMainThread(() => success = true);
+            Assert.IsTrue(success);
+        }
+    }
+}
