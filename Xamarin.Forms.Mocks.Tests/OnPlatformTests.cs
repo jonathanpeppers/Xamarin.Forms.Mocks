da87ca9b4c264554fd9efa495136728608fa31d3
Jonathan Peppers <jonathan.peppers@gmail.com>
2017-05-04 15:58:54 -0500

    OnPlatform<T> mocking support

---
+++ Xamarin.Forms.Mocks.Tests/OnPlatformTests.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+
+namespace Xamarin.Forms.Mocks.Tests
+{
+    [TestFixture]
+    public class OnPlatformTests
+    {
+        [Test]
+        public void DefaultString()
+        {
+            MockForms.Init();
+
+            var platformString = new OnPlatform<string>();
+            platformString.Platforms.Add(new On
+            {
+                Value = "Chuck",
+                Platform = new[] { Device.RuntimePlatform }
+            });
+
+            Assert.AreEqual("Chuck", (string)platformString);
+        }
+
+        [Test]
+        public void DefaultColor()
+        {
+            MockForms.Init();
+
+            var platformColor = new OnPlatform<Color>();
+            platformColor.Platforms.Add(new On
+            {
+                Value = Color.Red,
+                Platform = new[] { Device.RuntimePlatform }
+            });
+
+            Assert.AreEqual(Color.Red, (Color)platformColor);
+        }
+
+        [Test]
+        public void iOSColor()
+        {
+            MockForms.Init(Device.iOS);
+
+            var platformColor = new OnPlatform<Color>();
+            platformColor.Platforms.Add(new On
+            {
+                Value = Color.Red,
+                Platform = new[] { Device.iOS }
+            });
+
+            Assert.AreEqual(Color.Red, (Color)platformColor);
+        }
+
+        [Test]
+        public void AndroidColor()
+        {
+            MockForms.Init(Device.Android);
+
+            var platformColor = new OnPlatform<Color>();
+            platformColor.Platforms.Add(new On
+            {
+                Value = Color.Red,
+                Platform = new[] { Device.Android }
+            });
+
+            Assert.AreEqual(Color.Red, (Color)platformColor);
+        }
+
+        [Test]
+        public void WindowsColor()
+        {
+            MockForms.Init(Device.Windows);
+
+            var platformColor = new OnPlatform<Color>();
+            platformColor.Platforms.Add(new On
+            {
+                Value = Color.Red,
+                Platform = new[] { Device.Windows }
+            });
+
+            Assert.AreEqual(Color.Red, (Color)platformColor);
+        }
+
+        [Test]
+        public void WinPhoneColor()
+        {
+            MockForms.Init(Device.WinPhone);
+
+            var platformColor = new OnPlatform<Color>();
+            platformColor.Platforms.Add(new On
+            {
+                Value = Color.Red,
+                Platform = new[] { Device.WinPhone }
+            });
+
+            Assert.AreEqual(Color.Red, (Color)platformColor);
+        }
+    }
+}
