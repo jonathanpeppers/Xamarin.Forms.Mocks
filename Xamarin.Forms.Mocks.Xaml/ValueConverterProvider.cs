ddd914476bc04c1b409697115285500497573b20
Jonathan Peppers <jonathan.peppers@gmail.com>
2017-05-04 15:58:27 -0500

    IValueConverterProvider implementation

---
+++ Xamarin.Forms.Mocks.Xaml/ValueConverterProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Mocks.Xaml
+{
+    public class ValueConverterProvider : IValueConverterProvider
+    {
+        public object Convert(object value, Type toType, Func<MemberInfo> minfoRetriever, IServiceProvider serviceProvider)
+        {
+            return value.ConvertTo(toType, minfoRetriever, serviceProvider);
+        }
+    }
+}
