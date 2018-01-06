8be895d2c8a44f49597fd100ea38aaa15dd754d3
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-01-06 01:00:36 -0600

    [test] added a test for NativeViews
    Related to #18
    
    I used sample code from here: https://developer.xamarin.com/guides/xamarin-forms/platform-features/native-views/#Consuming_Native_Views

--- Xamarin.Forms.Mocks.Tests/LoadFromXamlTests.cs
+++ Xamarin.Forms.Mocks.Tests/LoadFromXamlTests.cs
@@ -43,5 +43,30 @@ namespace Xamarin.Forms.Mocks.Tests
             var label = (Label)view.Content;
             Assert.AreEqual("Compiled", label.Text);
         }
+
+        [Test]
+        public void NativeViews()
+        {
+            var page = new ContentPage();
+            page.LoadFromXaml(@"
+<ContentPage xmlns=""http://xamarin.com/schemas/2014/forms""
+        xmlns:x=""http://schemas.microsoft.com/winfx/2009/xaml""
+        xmlns:ios=""clr-namespace:UIKit;assembly=Xamarin.iOS;targetPlatform=iOS""
+        xmlns:androidWidget=""clr-namespace:Android.Widget;assembly=Mono.Android;targetPlatform=Android""
+        xmlns:formsAndroid=""clr-namespace:Xamarin.Forms;assembly=Xamarin.Forms.Platform.Android;targetPlatform=Android""
+        xmlns:win=""clr-namespace:Windows.UI.Xaml.Controls;assembly=Windows, Version=255.255.255.255,
+            Culture=neutral, PublicKeyToken=null, ContentType=WindowsRuntime;targetPlatform=Windows""
+        x:Class=""NativeViews.NativeViewDemo"">
+    <StackLayout Margin=""20"">
+        <ios:UILabel Text=""Hello World"" TextColor=""{x:Static ios:UIColor.Red}"" View.HorizontalOptions=""Start"" />
+        <androidWidget:TextView Text=""Hello World"" x:Arguments=""{x:Static formsandroid:Forms.Context}"" />
+        <win:TextBlock Text=""Hello World"" />
+    </StackLayout>
+</ContentPage>");
+
+            var layout = page.Content as StackLayout;
+            Assert.IsNotNull(layout);
+            Assert.AreEqual(new Thickness(20), layout.Margin);
+        }
     }
 }
